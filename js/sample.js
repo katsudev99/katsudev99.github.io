3b7d940d32634d08606cf5df29b3522a1f56dbdd
katsudev99 <katsu4509.ngu@gmail.com>
2021-01-10 01:16:02 +0900

    top page update

--- js/sample.js
+++ js/sample.js
@@ -1,1 +1,11 @@
-document.getElementById ('p1').innerHTML = 'Hello world ! ! !';
+function ColorChange () {
+  // Class「item」を持つ、すべての要素を配列で取得する。
+  var items = document.getElementsByClassName ('item');
+  // カラーピッカーの値を取得する。
+  var color = document.getElementById ('color');
+
+  // 取得した要素を1つずつ取り出し、背景色の変更を行う。
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = color.value;
+  }
+}
